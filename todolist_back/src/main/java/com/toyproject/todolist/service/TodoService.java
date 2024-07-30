ddb46edc43aa66a5b94294b32de0c2329b7342a1
gyeolhwi <gyeori0626@gmail.com>
2024-07-30 10:39:00 +0900

    주석추가

--- todolist_back/src/main/java/com/toyproject/todolist/service/TodoService.java
+++ todolist_back/src/main/java/com/toyproject/todolist/service/TodoService.java
@@ -12,7 +12,9 @@ public interface TodoService {
      // select 리스트 유저의 text
      List<TodoDto.RespDto> getListAll(int userid);
 
+     // update 수정(id,수정 할 text)
      int updateTodo(ReqUpdateTodoDto reqDto);
 
+     // delete 삭제(todo_id)
      int deleteTodo(int todo_id);
 }
